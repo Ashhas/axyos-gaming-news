aad4ebdddd48f22d2d86889a6238d58796353cf6
Aschwin B <aschwinbruyning@outlook.com>
2021-08-25 01:04:56 +0200

    + RecycleView Adapter

--- app/src/main/java/com/example/gamingnews/ui/MainActivity.kt
+++ app/src/main/java/com/example/gamingnews/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.gamingnews.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gamingnews.data.RssFeed
 import com.example.gamingnews.databinding.ActivityMainBinding
+import com.example.gamingnews.ui.adapter.ArticleAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -16,7 +20,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mainViewModel.getAllGematsuArticles()
+        val articleItems = mainViewModel.getAllGematsuArticles()
 
+        //RecycleView Adapter
+        binding.rvArticles.adapter = ArticleAdapter(
+            context = this,
+            articles = mainViewModel.sourceFeed?.itemList
+        )
+        binding.rvArticles.layoutManager = LinearLayoutManager(this)
+
+        //RadioButtons
+        binding.radioGroupSourceSelection.setOnCheckedChangeListener { _, checkId ->
+            if (checkId == binding.radioButtonGematsu.id) {
+                mainViewModel.getAllGematsuArticles()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/gamingnews/ui/adapter/ArticleAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.gamingnews.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamingnews.data.RssItem
+import com.example.gamingnews.databinding.ItemArticleBinding
+
+class ArticleAdapter(private val context: Context, private val articles: List<RssItem>?) :
+    RecyclerView.Adapter<ArticleAdapter.ArticleHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleHolder {
+        val view = ItemArticleBinding.inflate(
+            LayoutInflater.from(context),
+            parent,
+            false
+        )
+        return ArticleHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ArticleAdapter.ArticleHolder, position: Int) {
+        val article: RssItem? = articles?.get(position)
+        holder.bind(article)
+    }
+
+    override fun getItemCount(): Int {
+        if (articles != null) {
+            return articles.size
+        }
+        return 0
+    }
+
+    inner class ArticleHolder(private val itemBinding: ItemArticleBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(article: RssItem?) {
+            itemBinding.tvArticleName.text = article?.itemTitle
+            itemBinding.tvArticleDescription.text = article?.itemLink
+        }
+
+    }
+}
